7d2ad59e224b2618ec9897f230bf04186ebe491b
Alex Leschenko <leschenko.al@gmail.com>
2012-01-24 08:04:17 +0200

    orinal repo naming

--- lib/active_admin/resource/naming.rb
+++ lib/active_admin/resource/naming.rb
@@ -1,23 +1,14 @@
 module ActiveAdmin
   class Resource
     module Naming
-
-      def name_of_resource_class
-        @options[:as] || resource_class.name.gsub('::', '')
-      end
-
-      def plural_name_of_resource_class
-        name_of_resource_class.pluralize
-      end
-
-      # Returns the name to call this resource such as "Bank Account"
+      # Returns the user facing name. Example: "Bank Account"
       def resource_name
         @resource_name ||= @options[:as]
         @resource_name ||= singular_human_name
-        @resource_name ||= resource_class.name.gsub('::',' ')
+        @resource_name ||= safe_resource_name
       end
 
-      # Returns the plural version of this resource such as "Bank Accounts"
+      # Returns the plural version of the user facing name. Example: "Bank Accounts"
       def plural_resource_name
         @plural_resource_name ||= @options[:as].pluralize if @options[:as]
         @plural_resource_name ||= plural_human_name
@@ -29,13 +20,22 @@ module ActiveAdmin
         camelized_resource_name
       end
 
+      def safe_resource_name
+        @safe_resource_name ||= @options[:as]
+        @safe_resource_name ||= resource_class.name.gsub('::', ' ')
+      end
+
+      def plural_safe_resource_name
+        safe_resource_name.pluralize
+      end
+
       # A camelized safe representation for this resource
       def camelized_resource_name
-        name_of_resource_class.titleize.gsub(' ', '')
+        safe_resource_name.titleize.gsub(' ', '')
       end
 
       def plural_camelized_resource_name
-        plural_name_of_resource_class.titleize.gsub(' ', '')
+        plural_safe_resource_name.titleize.gsub(' ', '')
       end
 
       # An underscored safe representation internally for this resource
@@ -47,6 +47,7 @@ module ActiveAdmin
       def plural_underscored_resource_name
         plural_camelized_resource_name.underscore
       end
+
 
       private
 
@@ -61,8 +62,7 @@ module ActiveAdmin
         return nil unless resource_class.respond_to?(:model_name)
 
         begin
-          resource_class.model_name.human
-          #I18n.translate!("activerecord.models.#{resource_class.model_name.underscore}.other").titleize
+          I18n.translate!("activerecord.models.#{resource_class.model_name.underscore}.other").titleize
         rescue I18n::MissingTranslationData
           nil
         end
